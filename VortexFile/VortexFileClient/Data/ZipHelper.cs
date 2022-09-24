9aff70c6cff56946ecc48d6637220ab8478f5238
ReyRom <s03roman@mail.ru>
2022-09-24 16:02:01 +0300

    zip

---
+++ VortexFile/VortexFileClient/Data/ZipHelper.cs
@@ -0,0 +1,65 @@
+﻿using Ionic.Zip;
+using Ionic.Zlib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace VortexFileClient.Data
+{
+    public static class ZipHelper
+    {
+        public static void CompressionDirectory(string fileName,
+            string sourceDirectory,
+            CompressionLevel compressionLevel = CompressionLevel.Default)
+        {
+            using (var zipFile = new ZipFile(fileName))
+            {
+                zipFile.CompressionLevel = compressionLevel;
+                zipFile.AddDirectory(sourceDirectory, "\\");
+                zipFile.Save();
+            }
+        }
+
+        public static void AppendFilesToZip(string fileName,
+            List<string> appendFiles,
+            CompressionLevel compressionLevel = CompressionLevel.Default)
+        {
+            using (var zipFile = ZipFile.Read(fileName))
+            {
+                zipFile.CompressionLevel = compressionLevel;
+                zipFile.AddFiles(appendFiles, "\\");
+                zipFile.Save();
+            }
+        }
+
+        public static void AppendFilesToZipFromArray(string fileName,
+            List<byte[]> appendFiles,
+            CompressionLevel compressionLevel = CompressionLevel.Default)
+        {
+            using (var zipFile = new ZipFile(fileName))
+            {
+                zipFile.CompressionLevel = compressionLevel;
+                foreach (var file in appendFiles)
+                {
+                    var tempFileName = Guid.NewGuid().ToString();
+                    zipFile.AddEntry(tempFileName, file);
+                }
+
+                zipFile.Save();
+            }
+        }
+
+        public static void ExtractZip(string fileName, string outFolder)
+        {
+            using (var zip = ZipFile.Read(fileName))
+            {
+                foreach (var e in zip)
+                {
+                    e.Extract(outFolder, ExtractExistingFileAction.OverwriteSilently);
+                }
+            }
+        }
+    }
+}
